71cc2d9f7b83a414bd3a773406c8e89fb59d919e
Vectari <mateusz.majer996@gmail.com>
2022-11-13 19:57:08 +0100

    add remove playlist element

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
       playlistTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4}, {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5}, {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}]
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
@@ -26,6 +27,13 @@ class App extends React.Component {
     this.setState({ playlistTracks:tracks })
   }
 
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
+  }
+
   render() {
     return (
       <div>
@@ -36,7 +44,8 @@ class App extends React.Component {
             <SearchResults searchResults={this.state.searchResults} 
                                   onAdd = {this.addTrack} />
             <Playlist plalistName = {this.state.playlistName} 
-                      playlistTracks = {this.state.playlistTracks} />
+                      playlistTracks = {this.state.playlistTracks} 
+                      onRemove = {this.removeTrack} />
           </div>
         </div>
       </div>
